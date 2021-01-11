967cd2bf5b2cb5e7b2b92d763a69f8b2c98e8af4
Wim Leflere <wim.leflere@gmail.com>
2021-01-11 19:37:27 +0100

    [libc++][P1679] string contains fix tabs instead of spaces

--- libcxx/test/std/strings/basic.string/string.contains/contains.string_view.pass.cpp
+++ libcxx/test/std/strings/basic.string/string.contains/contains.string_view.pass.cpp
@@ -89,7 +89,7 @@ constexpr bool test()
 int main(int, char**)
 {
     test();
-	// FIXME: wait for constexpr std::string
+    // FIXME: wait for constexpr std::string
     // static_assert(test());
 
     return 0;
